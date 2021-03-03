651a8b752f2621291f99807d54f455abe5df4b6f
Monica Tuominen <monica.tuominen@gmail.com>
2021-03-03 12:41:10 +0200

    added list

--- index.js
+++ index.js
@@ -14,19 +14,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+// http://localhost:8080/index.html
 app.use(express.static("public"));
 
-let customers = [
-    {id: 1, name: "Ada"},
-    {id: 2, name: "Bea"},
-    {id: 3, name: "Casper"}
-]
+let locations = [
+    {id: 1, latitude: 60, longitude: 70},
+    {id: 2, latitude: 40, longitude: 80}
+];
 
 
-
-//HTTP GET http://localhost:8080/api/customers
-app.get("/api/customers", (req, res) => {
-    res.send(customers);
+//HTTP GET http://localhost:8080/api/locations
+app.get("/api/locations", (req, res) => {
+    //pretty printin 
+    var str = JSON.stringify(locations, null, 2);
+  //  res.type("application/json");
+    res.send(str);
 })
 
 app.listen(port, () => {
